7c93c07dde1b54e8b010bcda5c919d4667aa7320
Perry <petreli1@ymail.com>
2022-11-10 16:28:53 +0000

    updated tests

--- src/tests/components/LocationDetails.test.js
+++ src/tests/components/LocationDetails.test.js
@@ -2,11 +2,28 @@ import React from "react";
 import { render } from "@testing-library/react";
 import LocationDetails from "../../components/LocationDetails";
 
-xdescribe("LocationDetails", () => {
+describe("LocationDetails", () => {
   const location = "Leeds, GB";
-  it("renders the correct city and location props", () => {
+
+  test("snapshot", () => {
+    const { asFragment } = render(<LocationDetails location={location} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("correctly renders props", () => {
     const { getByText } = render(<LocationDetails location={location} />);
 
-    expect(getByText("Leeds, GB")).toBeInstanceOf(HTMLHeadingElement);
+    expect(getByText(location)).toBeInstanceOf(HTMLHeadingElement);
+  });
+
+  test("if passed no falsey error message - renders that instead of location", () => {
+    const errMessage = "Oopsies an error";
+    const { queryByText } = render(
+      <LocationDetails location={location} errMessage={errMessage} />
+    );
+
+    expect(queryByText(errMessage)).toBeInstanceOf(HTMLHeadingElement);
+    expect(queryByText(location)).not.toBeInTheDocument();
   });
 });
