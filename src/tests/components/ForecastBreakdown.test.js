69677a2386020a5a401a086e062be189d1459c78
Perry <petreli1@ymail.com>
2022-11-10 12:31:54 +0000

    updated tests

--- src/tests/components/ForecastBreakdown.test.js
+++ src/tests/components/ForecastBreakdown.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ForecastBreakdown from "../../components/ForecastBreakdown";
+import getUnits from "../../helpers/getUnits";
 
 describe("ForecastBreakdown", () => {
   const validProps = {
@@ -13,62 +14,72 @@ describe("ForecastBreakdown", () => {
     weather: "cold",
   };
 
-  describe("snapshot", () => {
-    it("renders correctly", () => {
-      const { asFragment } = render(
-        <ForecastBreakdown
-          icon={validProps.icon}
-          temp={validProps.temp}
-          minTemp={validProps.minTemp}
-          maxTemp={validProps.maxTemp}
-          humidity={validProps.humidity}
-          windSpeed={validProps.windSpeed}
-          weather={validProps.weather}
-        />
-      );
+  const { icon, temp, minTemp, maxTemp, humidity, windSpeed, weather } =
+    validProps;
 
-      expect(asFragment()).toMatchSnapshot();
-    });
+  test("snapshot", () => {
+    const { asFragment } = render(
+      <ForecastBreakdown
+        icon={icon}
+        temp={temp}
+        minTemp={minTemp}
+        maxTemp={maxTemp}
+        humidity={humidity}
+        windSpeed={windSpeed}
+        weather={weather}
+      />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
   });
 
-  describe("props", () => {
-    it("renders correct values for props", () => {
-      const { getByText, getByTestId } = render(
-        <ForecastBreakdown
-          icon={validProps.icon}
-          temp={validProps.temp}
-          minTemp={validProps.minTemp}
-          maxTemp={validProps.maxTemp}
-          humidity={validProps.humidity}
-          windSpeed={validProps.windSpeed}
-          weather={validProps.weather}
-        />
-      );
+  test("correctly render props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastBreakdown
+        icon={icon}
+        temp={temp}
+        minTemp={minTemp}
+        maxTemp={maxTemp}
+        humidity={humidity}
+        windSpeed={windSpeed}
+        weather={weather}
+      />
+    );
 
-      expect(getByTestId("forecast-icon")).toHaveAttribute(
-        "class",
-        "forecast-breakdown__icon"
-      );
-      expect(getByText(validProps.temp)).toHaveAttribute(
-        "class",
-        "forecast-breakdown__temp"
-      );
-      expect(getByText(`${validProps.minTemp}K`)).toHaveAttribute(
-        "class",
-        "forecast-breakdown__min-temp"
-      );
-      expect(getByText(`${validProps.maxTemp}K`)).toHaveAttribute(
-        "class",
-        "forecast-breakdown__max-temp"
-      );
-      expect(getByText(`${validProps.humidity}%`)).toHaveAttribute(
-        "class",
-        "forecast-breakdown__humidity"
-      );
-      expect(getByText(`${validProps.windSpeed}m/s`)).toHaveAttribute(
-        "class",
-        "forecast-breakdown__wind-speed"
-      );
-    });
+    const { tempUnits, speedUnits } = getUnits();
+
+    expect(getByTestId("forecast-icon")).toContainHTML("i");
+    expect(getByText(`${temp}${tempUnits}`)).toBeInstanceOf(
+      HTMLParagraphElement
+    );
+    expect(getByText(`${minTemp}${tempUnits}`)).toBeInstanceOf(
+      HTMLParagraphElement
+    );
+    expect(getByText(`${maxTemp}${tempUnits}`)).toBeInstanceOf(
+      HTMLParagraphElement
+    );
+    expect(getByText(`${humidity}%`)).toBeInstanceOf(HTMLParagraphElement);
+    expect(getByText(`${windSpeed}${speedUnits}`)).toBeInstanceOf(
+      HTMLParagraphElement
+    );
+  });
+
+  test("it contains correct headings", () => {
+    const { getByText } = render(
+      <ForecastBreakdown
+        icon={icon}
+        temp={temp}
+        minTemp={minTemp}
+        maxTemp={maxTemp}
+        humidity={humidity}
+        windSpeed={windSpeed}
+        weather={weather}
+      />
+    );
+
+    expect(getByText(/min:/i)).toBeInstanceOf(HTMLHeadingElement);
+    expect(getByText(/max:/i)).toBeInstanceOf(HTMLHeadingElement);
+    expect(getByText(/humidity:/i)).toBeInstanceOf(HTMLHeadingElement);
+    expect(getByText(/wind speed:/i)).toBeInstanceOf(HTMLHeadingElement);
   });
 });
