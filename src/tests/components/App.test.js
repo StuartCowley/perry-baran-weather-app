0afac044d02061f4702b63aaf901871ca1662e19
Perry <petreli1@ymail.com>
2022-11-14 10:35:18 +0000

    removed unnecessary asnyc-await

--- src/tests/components/App.test.js
+++ src/tests/components/App.test.js
@@ -3,9 +3,8 @@ import { render } from "@testing-library/react";
 import App from "../../components/App";
 
 describe("App", () => {
-  test("snapshot", async () => {
-    const { asFragment } = await render(<App />);
-
+  test("snapshot", () => {
+    const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
